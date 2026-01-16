9b3eb3004a17314777c9b2d9e68730f8ca9e150b
strongest9 <gene.jaewoo@gmail.com>
2026-01-15 16:48:51 -0800

    Revert "Attempt to aggregate WM_MOUSEMOVE and use a buffer for WM_INPUT"
    This reverts commit f76700862d35de5b561446ba202e5868604df4b7.

--- platform/windows/display_server_windows.h
+++ platform/windows/display_server_windows.h
@@ -433,15 +433,6 @@ class DisplayServerWindows : public DisplayServer {
 		Rect2i parent_safe_rect;
 	};
 
-	// // frame counter
-	uint64_t last_mouse_event = UINT64_MAX;
-	// Buffer once per frame
-	uint64_t last_frame = UINT64_MAX;
-	// for Mouse
-	HWND mouse_hwnd;
-	WindowID mouse_window_id;
-	WPARAM mouse_wparam;
-
 	JoypadWindows *joypad = nullptr;
 	HHOOK mouse_monitor = nullptr;
 	List<WindowID> popup_list;
@@ -495,7 +486,6 @@ class DisplayServerWindows : public DisplayServer {
 
 	void _process_activate_event(WindowID p_window_id, WPARAM wParam, LPARAM lParam);
 	void _process_key_events();
-	LRESULT _process_mouse_events(HWND hWnd, WindowID window_id, WPARAM wParam);
 
 	static void _dispatch_input_events(const Ref<InputEvent> &p_event);
 	void _dispatch_input_event(const Ref<InputEvent> &p_event);
